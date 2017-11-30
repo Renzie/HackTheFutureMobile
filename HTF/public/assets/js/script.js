f00ea04a05bda2b6fb3805ff654f23c3be67f00c
Renzie <omanarenzie@gmail.com>
2017-11-30 14:47:31 +0100

    map

--- HTF/public/assets/js/script.js
+++ HTF/public/assets/js/script.js
@@ -27,3 +27,12 @@ function openQRCamera(node) {
 function showQRIntro() {
     return confirm("Use your camera to take a picture of a QR code.");
 }
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
