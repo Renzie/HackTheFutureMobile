539b275983bf228c08f9c93d892315469b50ba04
Yentis <yentl178@gmail.com>
2017-11-30 14:16:32 +0100

    fjsdklfjdls

--- HTF/public/assets/js/script.js
+++ HTF/public/assets/js/script.js
@@ -1,14 +1,12 @@
-
 $(function () {
     $('.tab-link').on('click',showTab)
 });
-
 
 function showTab(){
     console.log($(this).text());
     $('.tab').hide();
     $('.tab#'  + $(this).text()).fadeIn();
-};
+}
 
 function openQRCamera(node) {
     var reader = new FileReader();
